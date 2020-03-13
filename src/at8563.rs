75dfc0bbece73bf8b8d27bb2bda542927b8dd23a
Hideyuki Teramura <hide4d51@gmail.com>
2020-03-13 09:59:52 +0900

    Fix wrong trait bound syntax

--- src/at8563.rs
+++ src/at8563.rs
@@ -3,9 +3,9 @@ pub struct At8563<I2C> {
     addr: u8,
 }
 
-impl At8563<I2C> {
-    pub fn new(i2c: I2C) -> At8563<I2C>
+impl<I2C> At8563<I2C>
     where I2C: embedded_hal::blocking::i2c::Write {
+    pub fn new(i2c: I2C) -> At8563<I2C> {
         At8563 {
             i2c: i2c,
             addr: 0xA2,
