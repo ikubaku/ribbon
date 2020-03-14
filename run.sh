3d040cd7dfaffb212b60840bb9fe22a0445d7181
Hideyuki Teramura <hide4d51@gmail.com>
2020-03-14 19:28:00 +0900

    Update runner

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -m
-openocd -f interface/stlink-v2-1.cfg -f target/stm32f1x.cfg &> /dev/null &
+openocd -f interface/stlink-v2-1.cfg -f target/stm32f1x.cfg &> "openocd_$(date +%y%m%d%H%M_%s).log" &
 OPENOCD_PID=$!
 set +m
 gdb -x startup.gdb "$@"
