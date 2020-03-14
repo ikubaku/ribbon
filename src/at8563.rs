d9a9ff106be63d16da48b2c351221a5408542cd9
Hideyuki Teramura <hide4d51@gmail.com>
2020-03-14 15:31:50 +0900

    Add init code

--- src/at8563.rs
+++ src/at8563.rs
@@ -1,14 +1,24 @@
+use embedded_hal::blocking::i2c::{Write, Read};
+
 pub struct At8563<I2C> {
     i2c: I2C,
     addr: u8,
 }
 
 impl<I2C> At8563<I2C>
-    where I2C: embedded_hal::blocking::i2c::Write {
+where I2C: Write + Read,
+      <I2C as Write>::Error : core::fmt::Debug,
+      <I2C as Read>::Error : core::fmt::Debug {
     pub fn new(i2c: I2C) -> At8563<I2C> {
         At8563 {
             i2c: i2c,
             addr: 0xA2,
         }
+    }
+
+    pub fn init(&mut self) {
+        let payload = [0x02, 0x00] as [u8; 2];
+
+        self.i2c.write(self.addr, &payload).unwrap_or_else(|err| panic!("AT8563: Driver fatal error: {:#?}", err));
     }
 }
